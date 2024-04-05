3bacd7b412e324b4bd39fbc7ec05f0210fc02dd8
Bryce Moser <bryce.moser@wsu.edu>
2024-04-05 16:18:02 -0700

    Add router functionality to form submission

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next) {
+  console.log(req.body.firstname + " - " + req.body.lastname);
+  res.redirect("/submitted");
+});
+
 module.exports = router;
